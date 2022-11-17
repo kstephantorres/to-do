9ec86fb1b2b29e159960e93327a5a06be244c743
kstephantorres <kevinstephantorres@gmail.com>
2022-11-17 20:57:27 -0300

    clase02 completada

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn= document.querySelector("[data-form-btn]")
+
+const createTask = (event)=>{
+    event.preventDefault()
+    const input= document.querySelector("[data-form-input]")
+    console.log(input.value)
+    
+}
+
+btn.addEventListener('click', createTask)
